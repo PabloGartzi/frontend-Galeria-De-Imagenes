3bba8f8c8ec6a6e2de002e7335442983e447a347
zahirRC28 <zahirvalentino2812@gmail.com>
2025-11-07 23:01:12 +0100

    Establecer conexion y probarlo

---
+++ js/main.js
@@ -0,0 +1,47 @@
+/* VARIABLES*/
+const client = 'RQWQAsp0OfX6ei1TGzj7LPE2NronT34Begtc9VizPvO1kY3v9C7IUD2d';
+const urlApi = `https://api.pexels.com/v1/`
+
+/* EVENTOS*/
+
+/*FUNCIONES*/
+const connect = async (urlAp) => {
+    try {
+        const resp = await fetch(urlAp, {
+            method: 'GET', // el metodo para el llamado
+            headers: {
+                Authorization: client , //aqui invocamos el token
+            }
+        });
+        if(resp.ok){
+            const datos = await resp.json()
+            return datos
+        } else {
+            throw 'No hay'
+        }
+    } catch (error) {
+        throw (error + 'no encontramos nada')
+    }
+}
+
+const getimgprueba = async () => {
+    try {
+        const datos = await connect(`${urlApi}photos/2014422`)
+        //console.log(datos);
+    } catch (error) {
+        console.log(error);
+    }
+}
+const tag = `nature`
+const buscadorFotos = async (tag) => {
+    try {
+        const datos = await connect(`${urlApi}search?query=${tag}`)
+        const fotos = datos.photos
+        console.log(fotos)
+    } catch (error) {
+    }
+}
+
+/*INVOCACIONES*/
+getimgprueba();
+buscadorFotos();
